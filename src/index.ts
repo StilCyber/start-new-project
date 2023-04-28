a201f7e81cb1521910e94dae5c98abce03b6e2fd
stilCyber <karnozavr666@ya.ru>
2023-04-28 14:09:25 +0600

    lesson 3

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,70 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
-const port = 8888
 
-app.get('/', (req: any, res: any) => {
-   let helloMessage = 'Hello Incubator!!!';
-   res.send(helloMessage)
+const port = process.env.PORT || 8888
+
+const products = [{id: 1, title: 'tomato'}, {id: 2, title: 'orange'}]
+const adresses = [{id: 1, value: 'Nezalejnasti 12'}, {id: 2, value: 'Selickaga 11'}]
+
+const parserMiddleware = bodyParser({})
+app.use(parserMiddleware)
+
+app.get('/products', (req: Request, res: Response) => {
+   if (req.query.title) {
+      let searchString = req.query.title.toString()
+      res.send(products.filter(p => p.title.indexOf(searchString) > -1))
+   } else {
+      res.send(products)
+   }
+})
+app.delete('/products/:id', (req: Request, res: Response) => {
+   for (let i = 0; i < products.length; i++) {
+      if (products[i].id === +req.params.id) {
+         products.splice(i, 1)
+         res.send(204)
+         return
+      }
+   }
+   res.send(404)
+})
+
+app.post('/products', (req: Request, res: Response) => {
+   const newProduct = {
+      id: +(new Date()),
+      title: req.body.title
+   }
+   products.push(newProduct)
+   res.status(201).send(newProduct)
+})
+
+app.put('/products/:id', (req: Request, res: Response) => {
+   let product = products.find(p => p.id === +req.params.id)
+   if(product) {
+      product.title = req.body.title
+      res.send(product)
+   } else {
+      res.send(404)
+   }
+   
+})
+
+app.get('/adresses', (req: Request, res: Response) => {
+   res.send(adresses)
+})
+app.get('/adresses/:id', (req: Request, res: Response) => {
+   let adress = adresses.find(p => p.id === +req.params.id)
+   if(adress) {
+      res.send(adress)
+   } else {
+      res.send(404)
+   }
 })
 
 app.listen(port, () => {
-   console.log(`Example app listening on port ${port}`)
+   console.log(`Example app listening on port: ${port}`)
 })
+
+
+
